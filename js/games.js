20eba397c2957bd3a07d5185fe444ed03ec30472
markroze <markroze@gmail.com>
2015-11-26 12:35:56 +0200

    games page

--- js/games.js
+++ js/games.js
@@ -1,7 +1,23 @@
-// TODO: use ajax to get the file server/games.json
-// Create the list
+function renderGames (gamesJSON) {
+    $.getJSON( "lib/games.json", function( data ) {
+        var items = [];
+        $.each( data, function( key, val ) {
+            items.push( "<ul class='container container-" + key + "'><li class='class class-" + key + "'>" + val.name + "</li>" +
+                        "<li onclick="+"goto('" + val.url + "')" + "" +  " class='class imageClass class-" + key + "'><img src=" + val.picture + " /></li>" +
+                        "<li class='class class-" + key + "'>" + val.description + "</li>" +
+                        "</ul><br/>" );
+        });
 
-$.getJSON('server/games.json', function (games) {
-    console.log(games);
-})
+        $( "<ul/>", {
+            "class": "gamesList",
+           html: items.join( "" )
+        })
+            .appendTo( ".dataBox" );
+    });
+}
 
+$(document).ready(renderGames);
+
+function goto(url) {
+    window.location.assign(url);
+}
