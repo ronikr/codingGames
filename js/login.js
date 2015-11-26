77fd105b50ebc947ba5ef9c513abbc81e1171a06
liorhollinger <liorhollinger@gmail.com>
2015-11-26 12:12:38 +0200

    login changes

--- js/login.js
+++ js/login.js
@@ -1,17 +1,66 @@
-// TODO: handle remember me
-// TODO: encapsulate the storage
+
+var gLogedIn = false;
+
+$( document ).ready( showOrHideStats());
+
+
 
 function login() {
-    var secret = prompt("Know the secret baba?");
+    var elLogDiv = document.querySelector('.passField');
+    var elLogButton = document.querySelector('.logButton');
 
-    var a = "aaa";
-    if (secret === 'kama') {
-        sessionStorage.isAdmin = true;
+    if (!gLogedIn) {
+        elLogDiv.style.display = 'flex';
+        elLogButton.innerText = 'logout';
+        gLogedIn = true;
+
     }
-    alert(a);
+    else {
+        elLogDiv.style.display = 'none';
+        elLogButton.innerText = 'login';
+        sessionStorage.isAdmin = false;
+        localStorage.isAdmin = false;
+        gLogedIn = false;
+        var userName = document.querySelector('#userName');
+        var userPassword = document.querySelector('#password');
+        userName.value = '';
+        userPassword.value = '';
+        showOrHideStats();
+
+    }
 }
 
 // TODO: Handle logout
 function logout() {
 
+}
+
+function checkIfAdmin (event){
+    event.preventDefault();
+    var userName = document.querySelector('#userName');
+    var userPassword = document.querySelector('#password');
+    var adminUserName = 'yaron';
+    var adminPassWord = 'biton';
+    if (userName.value === adminUserName && userPassword.value === adminPassWord){
+        sessionStorage.isAdmin = true;
+        handleStorage('isAdmin',true);
+        window.open("welcome.html");
+        showOrHideStats ();
+    }
+}
+function handleStorage(name,value){
+    sessionStorage[name] = value;
+    var elRememberMe =  document.querySelector('#rememberMe');
+    if (elRememberMe.checked){
+        localStorage[name]= value;
+    }
+}
+function showOrHideStats (){
+    var elAdminStats = document.querySelector('.admin-stats');
+    if (sessionStorage.isAdmin === 'true') {
+        elAdminStats.style.display = 'flex';
+    }
+    else{
+        elAdminStats.style.display = 'none';
+    }
 }
